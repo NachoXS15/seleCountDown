dd9db5ce8f26c2a766a10fc4a69c4c47d2fbc5b9
NachoXS-NET <pantojaignacio642@gmail.com>
2024-06-29 22:01:03 -0300

    created first styles

---
+++ script.js
@@ -0,0 +1,5 @@
+const months = document.getElementById("months")
+const days = document.getElementById("days")
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("months")
