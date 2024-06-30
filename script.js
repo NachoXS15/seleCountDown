9875aefaa8574d5c954b86a135bb27c50ca1306e
NachoXS-NET <pantojaignacio642@gmail.com>
2024-06-29 22:06:06 -0300

    creating function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const months = document.getElementById("months")
-const days = document.getElementById("days")
-const hours = document.getElementById("hours")
-const minutes = document.getElementById("minutes")
-const seconds = document.getElementById("months")
+const eMonths = document.getElementById("months")
+const eDays = document.getElementById("days")
+const eHours = document.getElementById("hours")
+const eMinutes = document.getElementById("minutes")
+const eSeconds = document.getElementById("months")
+
+const dateTarget = new Date("July 20, 2024 16:00:00").getTime();
+
+function countDown(){
+    const now = Date.now
+    const diference = dateTarget - now;
+    console.log(diference);
+}
+
+window.onload(() => {
+    countDown()
+})
