f48604b6089589189fec2c653465bd12e1d97c4d
NachoXS-NET <pantojaignacio642@gmail.com>
2024-06-30 12:02:27 -0300

    created countdown

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ const eMonths = document.getElementById("months")
 const eDays = document.getElementById("days")
 const eHours = document.getElementById("hours")
 const eMinutes = document.getElementById("minutes")
-const eSeconds = document.getElementById("months")
+const eSeconds = document.getElementById("seconds")
 
-const dateTarget = new Date("July 20, 2024 16:00:00").getTime();
+const targetDate = new Date("July 20, 2024 16:00:00").getTime();
 
-function countDown(){
-    const now = Date.now
-    const diference = dateTarget - now;
-    console.log(diference);
+function updateCountdown() {
+    const now = new Date().getTime().toFixed()
+    const distance = targetDate - now;
+    console.log(distance);
+    if (distance < 0) {
+        clearInterval(interval);
+        daysElement.innerText = '0';
+        hoursElement.innerText = '0';
+        minutesElement.innerText = '0';
+        secondsElement.innerText = '0';
+        return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    eDays.innerText = days;
+    eHours.innerText = hours;
+    eMinutes.innerText = minutes;
+    eSeconds.innerText = seconds;
 }
 
-window.onload(() => {
-    countDown()
-})
+window.onload = function() {
+    updateCountdown();
+    setInterval(updateCountdown, 1000);
+};
